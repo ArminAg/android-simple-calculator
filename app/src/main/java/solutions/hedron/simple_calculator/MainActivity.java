93e30eeef498c626bbd09ae8b44af4e551cf23f7
Armin <armin.ag@outlook.com>
2017-01-24 22:46:11 +0100

    Bind number press events

--- app/src/main/java/solutions/hedron/simple_calculator/MainActivity.java
+++ app/src/main/java/solutions/hedron/simple_calculator/MainActivity.java
@@ -1,7 +1,9 @@
 package solutions.hedron.simple_calculator;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView resultView;
+    String currentNumber = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +43,72 @@ public class MainActivity extends AppCompatActivity {
 
         // Clear Button
         Button clearBtn = (Button)findViewById(R.id.clearButton);
+
+        // Bind Number Events
+        oneBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(1);
+            }
+        });
+        twoBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(2);
+            }
+        });
+        threeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(3);
+            }
+        });
+        fourBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(4);
+            }
+        });
+        fiveBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(5);
+            }
+        });
+        sixBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(6);
+            }
+        });
+        sevenBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(7);
+            }
+        });
+        eightBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(8);
+            }
+        });
+        nineBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(9);
+            }
+        });
+        zeroBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                numberPressed(0);
+            }
+        });
+    }
+
+    void numberPressed(int number){
+        currentNumber += String.valueOf(number);
+        resultView.setText(currentNumber);
     }
 }
